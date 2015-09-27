13370b1baf980309af86aaa4b4f8e93835e48689
leetenki <toukyouniversityoftechnology@gmail.com>
2015-09-28 04:06:53 +0900

    fix a typo

--- db/migrate/20150925101441_add_price_to_trader.rb
+++ db/migrate/20150925101441_add_price_to_trader.rb
@@ -4,7 +4,7 @@ class AddPriceToTrader < ActiveRecord::Migration
     add_column :traders, :group_price_indivisual, :integer, :default => 0
     add_column :traders, :group_price_1_10, :integer, :default => 0
     add_column :traders, :group_price_11_20, :integer, :default => 3000
-    add_column :traders, :group_price_21_30, :intger, :default => 6000
+    add_column :traders, :group_price_21_30, :integer, :default => 6000
     add_column :traders, :group_price_31_40, :integer, :default => 9000
     add_column :traders, :year_3_price, :integer, :default => 1000
     add_column :traders, :year_5_price, :integer, :default => 1000
