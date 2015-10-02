297339e651c8427cfba1a53078cd389c2ff27e0c
leetenki <toukyouniversityoftechnology@gmail.com>
2015-10-02 21:23:44 +0900

    change logger

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,7 +53,7 @@ Rails.application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups.
-  config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = ActiveSupport::TaggedLogging.new("log/production.log", 'monthly')
 
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
