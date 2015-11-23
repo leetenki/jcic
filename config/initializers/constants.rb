6a443125e5544137bfd98c3c1ec602b47a4e1957
leetenki <toukyouniversityoftechnology@gmail.com>
2015-11-23 15:06:34 +0900

    change order of constants.rb

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -4,8 +4,8 @@ module Constants
   VISA_TYPE_TABLE = {"individual" => "个人查证", "group" => "团体查证", "3years" => "三年多次", "5years" => "五年多次"}
 
   #身元保証書用 & 自動入力用
-  GROUP_VISA = [:jki, :jcic, :jtg]
-  INDIVIDUAL_VISA = [:jki, :jtg] #:jki
+  GROUP_VISA = [:jtg, :jki, :jcic]
+  INDIVIDUAL_VISA = [:jtg, :jki] #:jki
   JAPAN_SIDE_COMPANY = {
     :jtg => {
       :company_name => "ジェーティージー華信株式会社",
