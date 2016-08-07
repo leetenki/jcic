c71eb6b1b24e326f7a8c2c4f50e44da7f2bbab55
leetenki <toukyouniversityoftechnology@gmail.com>
2016-08-08 03:55:50 +0900

    remove index from migrate

--- db/migrate/20160807172952_create_payoffs.rb
+++ db/migrate/20160807172952_create_payoffs.rb
@@ -1,7 +1,7 @@
 class CreatePayoffs < ActiveRecord::Migration
   def change
     create_table :payoffs do |t|
-      t.references :trader, index: true, foreign_key: true
+      t.references :trader, foreign_key: true
       t.string :status
 
       t.timestamps null: false
