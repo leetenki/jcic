c383fd4c5c18ee6c4b17a33126a09a367630ec86
tianqi-li <tianqi.li@dena.com>
2017-05-08 19:35:43 +0900

    change 3, 5 year visa name

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 module Constants
-  VISA_TYPE_TABLE = {"individual" => "个人查证", "group" => "团体查证", "3years" => "三年多次", "5years" => "五年多次"}
+  VISA_TYPE_TABLE = {"individual" => "个人查证", "group" => "团体查证", "3years" => "冲绳东北六县多次", "5years" => "一定经济能力多次"}
 
   #身元保証書用 & 自動入力用
   GROUP_VISA = [:jtg, :jki, :jcic]
